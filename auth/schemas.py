71de5153642ef514e4485c6d3cbde0272ce40596
MikhailShurov <mixailshurov@gmail.com>
2024-04-14 18:54:00 +0300

    Initial commit

---
+++ auth/schemas.py
@@ -0,0 +1,36 @@
+from datetime import date
+from typing import Optional
+
+from fastapi_users import schemas
+from pydantic import EmailStr
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: str
+    account_created: date
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    class Config:
+        from_attributes = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    id: int
+    email: str
+    password: str
+    account_created: date
+
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    password: Optional[str] = None
+    email: Optional[EmailStr] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_verified: Optional[bool] = None
